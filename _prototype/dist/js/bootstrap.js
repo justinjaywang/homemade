11874c4c6acae4fa78269ac98de306679a491638
Justin Wang <justinjaywang@gmail.com>
2014-05-09 11:44:29 -0700

    Change to Helvetica Neue and PT Serif; Added more elements to post-description

--- _prototype/dist/js/bootstrap.js
+++ _prototype/dist/js/bootstrap.js
@@ -2140,15 +2140,6 @@ if (typeof jQuery === 'undefined') { throw new Error('Bootstrap\'s JavaScript re
         var imageTop = $image.offset().top;
         var imageBottom = imageTop + $image.outerHeight();
         $image.css('opacity', pos2Opacity(top, bottom, imageTop, imageBottom));
-        var $cover = $image.parent();
-        var $post = $cover.parent();
-        var colorCurr = $post.css('background-color');
-        var $postPrev = $post.prev();
-        var colorPrev = colorCurr;
-        if ($postPrev.length) { // if there is a previous post
-          colorPrev = $postPrev.css('background-color');
-        }
-        $cover.css('background-color', pos2Background(bottom, imageBottom, colorPrev, colorCurr));
       });
 
       $(settings.fader).each(function () {
@@ -2163,32 +2154,16 @@ if (typeof jQuery === 'undefined') { throw new Error('Bootstrap\'s JavaScript re
     var pos2Opacity = function(windowTop, windowBottom, elementTop, elementBottom) {
 
       var percentage;
-      // var elementMid = (4*elementBottom + elementTop) / 5;
-      var elementMid = elementBottom;
 
       if (windowBottom < elementTop) { // image not scrolled to
         return 0;
-      } else if (windowBottom < elementMid) { // image partly scrolled to
-        percentage = (windowBottom - elementTop) / (elementMid - elementTop);
+      } else if (windowBottom < elementBottom) { // image partly scrolled to
+        percentage = (windowBottom - elementTop) / (elementBottom - elementTop);
         return percentage;
-      // } else if (windowTop < elementTop) { // image in full sight
-      //   return 1;
-      // } else if (windowTop < elementBottom) { // image partly scrolled past
-      //   percentage = (windowTop - elementTop) / (elementBottom - elementTop);
-      //   return (1 - percentage);
       } else { // image scrolled past
-        // return 0;
         return 1;
       }
 
-    };
-
-    var pos2Background = function(windowBottom, elementBottom, colorPrev, colorCurr) {
-      if (windowBottom < elementBottom) {
-        return colorPrev;
-      } else {
-        return colorCurr;
-      }
     };
 
     var pos2FaderOpacity = function(windowTop, windowBottom, elementTop, elementBottom) {
--- _prototype/js/transitioner.js
+++ _prototype/js/transitioner.js
@@ -29,15 +29,6 @@
         var imageTop = $image.offset().top;
         var imageBottom = imageTop + $image.outerHeight();
         $image.css('opacity', pos2Opacity(top, bottom, imageTop, imageBottom));
-        var $cover = $image.parent();
-        var $post = $cover.parent();
-        var colorCurr = $post.css('background-color');
-        var $postPrev = $post.prev();
-        var colorPrev = colorCurr;
-        if ($postPrev.length) { // if there is a previous post
-          colorPrev = $postPrev.css('background-color');
-        }
-        $cover.css('background-color', pos2Background(bottom, imageBottom, colorPrev, colorCurr));
       });
 
       $(settings.fader).each(function () {
@@ -52,32 +43,16 @@
     var pos2Opacity = function(windowTop, windowBottom, elementTop, elementBottom) {
 
       var percentage;
-      // var elementMid = (4*elementBottom + elementTop) / 5;
-      var elementMid = elementBottom;
 
       if (windowBottom < elementTop) { // image not scrolled to
         return 0;
-      } else if (windowBottom < elementMid) { // image partly scrolled to
-        percentage = (windowBottom - elementTop) / (elementMid - elementTop);
+      } else if (windowBottom < elementBottom) { // image partly scrolled to
+        percentage = (windowBottom - elementTop) / (elementBottom - elementTop);
         return percentage;
-      // } else if (windowTop < elementTop) { // image in full sight
-      //   return 1;
-      // } else if (windowTop < elementBottom) { // image partly scrolled past
-      //   percentage = (windowTop - elementTop) / (elementBottom - elementTop);
-      //   return (1 - percentage);
       } else { // image scrolled past
-        // return 0;
         return 1;
       }
 
-    };
-
-    var pos2Background = function(windowBottom, elementBottom, colorPrev, colorCurr) {
-      if (windowBottom < elementBottom) {
-        return colorPrev;
-      } else {
-        return colorCurr;
-      }
     };
 
     var pos2FaderOpacity = function(windowTop, windowBottom, elementTop, elementBottom) {
