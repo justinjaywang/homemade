7b50e8c7eac02a4eeda342a185f7eb5ce9e682d6
Justin Wang <justinjaywang@gmail.com>
2014-05-09 22:31:20 -0700

    Cleaned up transitioner.js; Added text fade in and out

--- _prototype/dist/js/bootstrap.js
+++ _prototype/dist/js/bootstrap.js
@@ -2119,12 +2119,21 @@ if (typeof jQuery === 'undefined') { throw new Error('Bootstrap\'s JavaScript re
   'use strict';
 
   var defaults = {
-    image: '.post-cover-image',
-    fader: '.post-cover-fader',
-    fadeStart: 0,
-    fadeMid: 0.2,
-    fadeEnd: 0,
-    header: '.post-header'
+    postSelector: '.post',
+    coverSelector: '.post-cover',
+    imageSelector: '.post-cover-image',
+    faderSelector: '.post-cover-fader',
+    headerSelector: '.post-header',
+    detailsSelector: '.post-details',
+    faderOpacityStart: 0,
+    faderOpacityMid: 0.2,
+    faderOpacityEnd: 0,
+    imageOpacityStart: 0,
+    imageOpacityMid: 1,
+    imageOpacityEnd: 0,
+    headerBottomStart: 10,
+    headerBottomEnd: -4,
+    accel: 1.5
   };
 
   $.fn.transitioner = function (options) {
@@ -2136,118 +2145,95 @@ if (typeof jQuery === 'undefined') { throw new Error('Bootstrap\'s JavaScript re
       var top = $(window).scrollTop();
       var bottom = top + $(window).height();
 
-      $(settings.image).each(function () {
-        var $image = $(this);
-        var imageTop = $image.offset().top;
-        var imageBottom = imageTop + $image.outerHeight();
-        $image.css('opacity', pos2ImageOpacity(top, bottom, imageTop, imageBottom));
+      $(settings.postSelector).each(function () {
 
-        var $cover = $image.parent();
-        var $post = $cover.parent();
-        var colorCurr = $post.css('background-color'); // get color of current post
-        var colorPrev = colorCurr;
-        var $postPrev = $post.prev(); // default color of previous post to current
-        if ($postPrev.length) { // if there is a previous post
+        var $post = $(this),
+            $postPrev = $post.prev(),
+            $cover = $post.find(settings.coverSelector),
+            $image = $cover.find(settings.imageSelector),
+            $fader = $cover.find(settings.faderSelector),
+            $header = $cover.find(settings.headerSelector),
+            $details = $post.find(settings.detailsSelector);
+        
+        var postTop = $post.offset().top,
+            coverBottom = postTop + $image.outerHeight(),
+            postBottom = postTop + $post.outerHeight();
+
+        var colorCurr = $post.css('background-color'),
+            colorPrev = colorCurr;
+        if ($postPrev.length) {
           colorPrev = $postPrev.css('background-color');
         }
-        $cover.css('background-color', pos2CoverBackground(bottom, imageBottom, colorPrev, colorCurr));
 
-      });
+        // Fade in and out cover image and cover fader
+        $image.css('opacity', pos2CoverElementOpacity(top, bottom, postTop, coverBottom, settings.imageOpacityStart, settings.imageOpacityMid, settings.imageOpacityEnd));
+        $fader.css('opacity', pos2CoverElementOpacity(top, bottom, postTop, coverBottom, settings.faderOpacityStart, settings.faderOpacityMid, settings.faderOpacityEnd));
 
-      $(settings.fader).each(function () {
-        var $fader = $(this);
-        var faderTop = $fader.offset().top;
-        var faderBottom = faderTop + $fader.outerHeight();
-        $fader.css('opacity', pos2FaderOpacity(top, bottom, faderTop, faderBottom));
-      });
+        // Toggle cover background color
+        $cover.css('background-color', pos2CoverBackground(bottom, coverBottom, colorPrev, colorCurr));
+        
+        // Position header text
+        $header.css('bottom', pos2HeaderBottom(top, postTop, coverBottom) + '%');
+        
+        // Fade out text
+        $header.css('opacity', pos2TextOpacity(top, bottom, postTop, postBottom));
+        $details.css('opacity', pos2TextOpacity(top, bottom, postTop, postBottom));
 
-      $(settings.header).each(function () {
-        var $header = $(this);
-        // var headerBottom = $header.css('bottom');
-        var $cover = $header.parent();
-        var coverTop = $cover.offset().top;
-        var coverBottom = coverTop + $cover.outerHeight();
-        $header.css('bottom', pos2HeaderBottom(top, coverTop, coverBottom) + '%');
       });
 
     };
 
-    var pos2HeaderBottom = function(windowTop, coverTop, coverBottom) {
-      if (windowTop < coverTop) { // cover not scrolled to, partly scrolled to, or in full sight
-        return 10;
-      } else { // cover partly scrolled past
-        var percentage = (windowTop - coverTop) / (coverBottom - coverTop);
-        return Math.max((-14 * percentage*1.5) + 10, -4);
-      // } else { // cover scrolled past
-        // return -10;
+    var pos2CoverElementOpacity = function(windowTop, windowBottom, elementTop, elementBottom, opacityStart, opacityMid, opacityEnd) {
+
+      if (windowBottom < elementTop) { // image not scrolled to
+        return opacityStart;
+      } else if (windowBottom < elementBottom) { // image partly scrolled to
+        var percentScrolledTo = (windowBottom - elementTop) / (elementBottom - elementTop);
+        return Math.min((opacityMid - opacityStart) * percentScrolledTo * settings.accel + opacityStart, opacityMid);
+      } else if (windowTop < elementTop) { // image in full sight
+        return opacityMid;
+      } else if (windowTop < elementBottom) { // image partly scrolled past
+        var percentScrolledPast = (windowTop - elementTop) / (elementBottom - elementTop);
+        return Math.max((opacityEnd - opacityMid) * percentScrolledPast * settings.accel + opacityMid, opacityStart);
+      } else { // image scrolled past
+        return opacityEnd;
       }
+
     };
 
     var pos2CoverBackground = function(windowBottom, elementBottom, colorPrev, colorCurr) {
+
       if (windowBottom < elementBottom) {
         return colorPrev;
       } else {
         return colorCurr;
       }
+
     };
 
-    var pos2ImageOpacity = function(windowTop, windowBottom, elementTop, elementBottom) {
+    var pos2HeaderBottom = function(windowTop, coverTop, coverBottom) {
 
-      var percentage;
-
-      // if (windowBottom < elementTop) { // image not scrolled to
-      //   return 0;
-      // } else if (windowBottom < elementBottom) { // image partly scrolled to
-      //   percentage = (windowBottom - elementTop) / (elementBottom - elementTop);
-      //   return percentage;
-      // } else { // image scrolled past
-      //   return 1;
-      // }
-
-      // if (windowTop < elementTop) { // image in full sight
-      //   return 1;
-      // } else if (windowTop < elementBottom) { // image partly scrolled past
-      //   percentage = (windowTop - elementTop) / (elementBottom - elementTop);
-      //   return Math.max((1 - percentage*1.5), 0);
-      // } else { // image scrolled past
-      //   return 0;
-      // }
-
-      if (windowBottom < elementTop) { // image not scrolled to
-        return 0;
-      } else if (windowBottom < elementBottom) { // image partly scrolled to
-        percentage = (windowBottom - elementTop) / (elementBottom - elementTop);
-        return percentage;
-        // return Math.min(percentage*1.5, 1);
-      } else if (windowTop < elementTop) { // image in full sight
-        return 1;
-      } else if (windowTop < elementBottom) { // image partly scrolled past
-        percentage = (windowTop - elementTop) / (elementBottom - elementTop);
-        return Math.max((1 - percentage*1.5), 0);
-      } else { // image scrolled past
-        return 0;
+      if (windowTop < coverTop) { // cover not scrolled to, partly scrolled to, or in full sight
+        return settings.headerBottomStart;
+      } else if (windowTop < coverBottom) { // cover partly scrolled past
+        var percentScrolledPast = (windowTop - coverTop) / (coverBottom - coverTop);
+        return Math.max(((-1 * settings.headerBottomStart + settings.headerBottomEnd) * percentScrolledPast * settings.accel) + settings.headerBottomStart, settings.headerBottomEnd);
+      } else {
+        return settings.headerBottomEnd;
       }
 
     };
 
-    var pos2FaderOpacity = function(windowTop, windowBottom, elementTop, elementBottom) {
+    var pos2TextOpacity = function(windowTop, windowBottom, postTop, postBottom) {
+      
+      var windowMid = (windowTop + windowBottom) / 2;
 
-      var percentage;
-      // var elementMid = (4*elementBottom + elementTop) / 5;
-      // var elementMid = elementBottom;
-
-      if (windowBottom < elementTop) { // image not scrolled to
-        return settings.fadeStart;
-      } else if (windowBottom < elementBottom) { // image partly scrolled to
-        percentage = (windowBottom - elementTop) / (elementBottom - elementTop);
-        return percentage * (settings.fadeMid - settings.fadeStart) + settings.fadeStart;
-      } else if (windowTop < elementTop) { // image in full sight
-        return settings.fadeMid;
-      } else if (windowTop < elementBottom) { // image partly scrolled past
-        percentage = (windowTop - elementTop) / (elementBottom - elementTop);
-        return percentage * (settings.fadeEnd - settings.fadeMid) + settings.fadeMid;
-      } else { // image scrolled past
-        return settings.fadeEnd;
+      if (windowMid < postTop) { // not scrolled past halfway
+        return 0;
+      } else if (windowMid < postBottom) {
+        return 1;
+      } else {
+        return 0;
       }
 
     };
